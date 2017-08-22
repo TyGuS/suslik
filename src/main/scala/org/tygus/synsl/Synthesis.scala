eb3b5a9c78249cfad4ee8b3f5294d1f049051a87
Ilya Sergey <i.sergey@ucl.ac.uk>
2017-08-22 19:07:48 +0100

    Basic structure for synthesis

---
+++ src/main/scala/org/tygus/synsl/Synthesis.scala
@@ -0,0 +1,22 @@
+package org.tygus.synsl
+
+import org.tygus.synsl.language.Expressions._
+import org.tygus.synsl.language.Statements._
+import org.tygus.synsl.language.SynslType
+import org.tygus.synsl.logic.Specifications._
+
+/**
+  * @author Ilya Sergey
+  */
+
+object Synthesis {
+
+  type Gamma = Seq[(SynslType, Var)]
+
+  def synthesize(spec: Spec): Procedure = {
+
+    // TODO: implement Nadia's rules
+
+  }
+
+}
