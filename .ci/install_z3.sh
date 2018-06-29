b9cd124015fdc80ea76603438b8dd94f17fe26ca
Ilya Sergey <ilya.sergey@gmail.com>
2018-06-29 01:31:36 +0100

    an attempt to update Z3

--- .ci/install_z3.sh
+++ .ci/install_z3.sh
@@ -4,14 +4,14 @@ set -x
 
 version=4.6.0
 
-if [ ! -f "$HOME/z3/z3-${version}-x64-ubuntu-14.04/bin/z3" ]; then
+if [ ! -f "$HOME/z3/z3-${version}-x64-ubuntu-16.04/bin/z3" ]; then
   mkdir -p $HOME/z3
   if [ "$(ls -A $HOME/z3)" ]; then
       rm -r $HOME/z3/*
   fi
-  wget https://github.com/Z3Prover/z3/releases/download/z3-${version}/z3-${version}-x64-ubuntu-14.04.zip -O ~/z3.zip
+  wget https://github.com/Z3Prover/z3/releases/download/z3-${version}/z3-${version}-x64-ubuntu-16.04.zip -O ~/z3.zip
   unzip ~/z3.zip -d ~/z3
 fi
 
-PATH="$HOME/z3/z3-${version}-x64-ubuntu-14.04/bin/:$PATH"
+PATH="$HOME/z3/z3-${version}-x64-ubuntu-16.04/bin/:$PATH"
 z3 --version
