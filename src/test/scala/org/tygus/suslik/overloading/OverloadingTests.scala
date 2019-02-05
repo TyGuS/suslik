e0ed47f5781211781c0980cb4e26bad09dbf416b
Roman Shchedrin <rashchedrin@gmail.com>
2019-02-05 14:49:11 +0300

    Some tests

--- src/test/scala/org/tygus/suslik/overloading/OverloadingTests.scala
+++ src/test/scala/org/tygus/suslik/overloading/OverloadingTests.scala
@@ -1,20 +1,94 @@
 package org.tygus.suslik.synthesis
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{Matchers, FunSpec => scalatestFunSpec}
+import org.tygus.suslik.language.Expressions.Var
+import org.tygus.suslik.logic._
+import org.tygus.suslik.logic.Resolver.resolveProgram
+import org.tygus.suslik.logic.Specifications.makeNewGoal
+import org.tygus.suslik.parsing.SSLParser
 import org.tygus.suslik.synthesis.instances.PhasedSynthesis
+import org.tygus.suslik.language._
+import org.tygus.suslik.util.SynStatUtil
 
 /**
   * @author Roman Shchedrin
   */
 
-class OverloadedOperatorsTests extends FunSpec with Matchers with SynthesisRunnerUtil {
+class OverloadedOperatorsTests extends scalatestFunSpec with Matchers with SynthesisRunnerUtil {
 
   val synthesis: Synthesis = new PhasedSynthesis
+
+  def resolveFromSpec(testName: String, text: String, out: String = "nope", params: SynConfig = defaultConfig): Specifications.Goal = {
+    val parser = new SSLParser
+    val res = parser.parseGoal(text)
+    if (!res.successful) {
+      throw SynthesisException(s"Failed to parse the input:\n$res")
+    }
+    val prog = res.get
+    val (specs, env) = resolveProgram(prog)
+    if (specs.lengthCompare(1) != 0) {
+      throw SynthesisException("Expected a single synthesis goal")
+    }
+    val spec = specs.head
+    val FunSpec(name, _, formals, pre, post) = spec
+    val goal = makeNewGoal(pre, post, formals, name, env)
+    goal
+  }
+
 
   def doRun(testName: String, desc: String, in: String, out: String, params: SynConfig = defaultConfig): Unit =
     it(desc) {
       synthesizeFromSpec(testName, in, out, params)
     }
+
+  describe("Resolver tests") {
+    it("should respect function signature") {
+      val code =
+        """
+          {emp} void foo (int i, bool b, loc l, set s){ emp}
+        """
+
+      val goal = resolveFromSpec("foo", code)
+      assert(goal.gamma(Var("i")) == IntType)
+      assert(goal.gamma(Var("l")) == LocType)
+      assert(goal.gamma(Var("s")) == IntSetType)
+      assert(goal.gamma(Var("b")) == BoolType)
+      print(goal.gamma)
+    }
+
+    it("should resolve ambiguity as int by default") {
+      val code =
+        """
+          {emp} void foo (int v){ www==www;emp}
+        """
+
+      val goal = resolveFromSpec("foo", code)
+      assert(goal.gamma(Var("www")) == IntType)
+      print(goal.gamma)
+    }
+
+    it("should resolve type from equality chain") {
+      val code =
+        """
+          {bb == ((a/\b) == (b \/ a))/\cc==bb/\dd==cc
+          /\ i == 5 /\ j == i; emp}
+          void foo (int v)
+          { ee==dd /\ k == j;emp}
+        """
+
+      val goal = resolveFromSpec("foo", code)
+      assert(goal.gamma(Var("a")) == BoolType)
+      assert(goal.gamma(Var("b")) == BoolType)
+      assert(goal.gamma(Var("bb")) == BoolType)
+      assert(goal.gamma(Var("cc")) == BoolType)
+      assert(goal.gamma(Var("dd")) == BoolType)
+      assert(goal.gamma(Var("ee")) == BoolType)
+      assert(goal.gamma(Var("i")) == IntType)
+      assert(goal.gamma(Var("j")) == IntType)
+      assert(goal.gamma(Var("k")) == IntType)
+      print(goal.gamma)
+    }
+  }
 
   describe("Overloaded equality tests") {
     runAllTestsFromDir("overloaded-equality")
---
+++ src/test/scala/org/tygus/suslik/try_failing/FailingTests.scala
@@ -0,0 +1,25 @@
+package org.tygus.suslik.synthesis
+
+import org.scalatest.{FunSpec, Matchers}
+import org.tygus.suslik.synthesis.instances.PhasedSynthesis
+
+/**
+  * @author Roman Shchedrin
+  */
+
+class FailingTests extends FunSpec with Matchers with SynthesisRunnerUtil {
+
+  val synthesis: Synthesis = new PhasedSynthesis
+
+  def doRun(testName: String, desc: String, in: String, out: String, params: SynConfig = defaultConfig): Unit =
+    it(desc) {
+      synthesizeFromSpec(testName, in, out, params)
+    }
+
+    if(false) {
+      describe("Failing tests") {
+        runAllTestsFromDir("failing-examples")
+      }
+    }
+
+}
