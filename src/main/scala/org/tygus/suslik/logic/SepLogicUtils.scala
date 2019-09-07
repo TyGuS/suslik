1b717ea4353e34f1d02a901b61d089d4409f4bec
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-09-07 12:50:28 +0800

    Nadia's fix + some immutability examples are working

--- src/main/scala/org/tygus/suslik/logic/SepLogicUtils.scala
+++ src/main/scala/org/tygus/suslik/logic/SepLogicUtils.scala
@@ -106,7 +106,7 @@ trait SepLogicUtils extends PureLogicUtils {
       case SApp(pred, args, tag, mut, submut) => stuff.filter {
         case SApp(_pred, _args, _tag, _mut, _submut) =>
           _pred == pred && args.length == _args.length &&
-            tag == _tag && mut == _mut && MTag.checkLists(submut, _submut) // TODO [Immutability]
+            mut == _mut && MTag.checkLists(submut, _submut) // TODO [Immutability]
         case _ => false
       }
     }
