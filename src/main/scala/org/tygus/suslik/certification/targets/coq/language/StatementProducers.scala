44df27950c74d80ff390a78de42c5eddbb3ee99b
Yasunari Watanabe <yasuwata27@gmail.com>
2020-06-22 23:32:42 +0800

    Translate the program producers directly

---
+++ src/main/scala/org/tygus/suslik/certification/targets/coq/language/StatementProducers.scala
@@ -0,0 +1,100 @@
+package org.tygus.suslik.certification.targets.coq.language
+
+import org.tygus.suslik.certification.targets.coq.language.Expressions.CExpr
+import org.tygus.suslik.certification.targets.coq.language.Statements.{CIf, CSeqComp, CStatement}
+
+object StatementProducers {
+  type Kont = Seq[CStatement] => CStatement
+
+  sealed abstract class CStmtProducer {
+    val arity: Int
+    val fn: Kont
+
+    def apply(children: Seq[CStatement]): CStatement = {
+      assert(children.lengthCompare(arity) == 0, s"Producer expects $arity children and got ${children.length}")
+      fn(children)
+    }
+
+    def >>(p: CStmtProducer): CStmtProducer = {
+      ChainedCStmtProducer(this, p)
+    }
+
+    def partApply(s: CStatement): CStmtProducer = {
+      PartiallyAppliedCStmtProducer(this, s)
+    }
+
+    def simplify: CStmtProducer = this match {
+      case ChainedCStmtProducer(p1, IdCStmtProducer) => p1.simplify
+      case ChainedCStmtProducer(IdCStmtProducer, p2) => p2.simplify
+      case ChainedCStmtProducer(_, p2@ConstCStmtProducer(_)) => p2.simplify
+      case _ => this
+    }
+  }
+
+  case class ChainedCStmtProducer(p1: CStmtProducer, p2: CStmtProducer) extends CStmtProducer {
+    val arity: Int = p1.arity + p2.arity - 1
+    val fn: Kont = stmts => {
+      val (stmts1, stmts2) = stmts.splitAt(p1.arity)
+      val s = p1.fn(stmts1)
+      p2.fn(s +: stmts2)
+    }
+  }
+
+  case class PartiallyAppliedCStmtProducer(p: CStmtProducer, s: CStatement) extends CStmtProducer {
+    val arity: Int = p.arity - 1
+    val fn: Kont = stmts => {
+      p.apply(s +: stmts)
+    }
+  }
+
+  case object IdCStmtProducer extends CStmtProducer {
+    val arity: Int = 1
+    val fn: Kont = _.head
+  }
+
+  case class ConstCStmtProducer(s: CStatement) extends CStmtProducer {
+    val arity: Int = 0
+    val fn: Kont = _ => s
+  }
+
+  case class PrependCStmtProducer(s: CStatement) extends CStmtProducer {
+    val arity: Int = 1
+    val fn: Kont = stmts => CSeqComp(s, stmts.head).simplify
+  }
+
+  case class AppendCStmtProducer(s: CStatement) extends CStmtProducer {
+    val arity: Int = 1
+    val fn: Kont = stmts => CSeqComp(stmts.head, s).simplify
+  }
+
+  case class BranchCStmtProducer(selectors: Seq[CExpr]) extends CStmtProducer {
+    val arity: Int = selectors.length
+    val fn: Kont = stmts =>
+      if (stmts.length == 1) stmts.head else {
+        val cond_branches = selectors.zip(stmts).reverse
+        val ctail = cond_branches.tail
+        val finalBranch = cond_branches.head._2
+        ctail.foldLeft(finalBranch) { case (eb, (c, tb)) => CIf(c, tb, eb).simplify }
+      }
+  }
+
+  case class FoldCStmtProducer[T](op: (T, CStmtProducer) => CStatement, item: T, bp: CStmtProducer) extends CStmtProducer {
+    val arity: Int = 1
+    val fn: Kont = steps => {
+      // partially apply a produced step to the BranchProducer of the downstream `bp`
+      @scala.annotation.tailrec
+      def isBase(curr: CStmtProducer): Boolean = curr match {
+        case PartiallyAppliedCStmtProducer(p, _) => isBase(p)
+        case _: BranchCStmtProducer => true
+        case _ => false
+      }
+      def update(curr: CStmtProducer): CStmtProducer = curr match {
+        case FoldCStmtProducer(op, item, bp) => FoldCStmtProducer(op, item, update(bp))
+        case ChainedCStmtProducer(p1, p2) => ChainedCStmtProducer(update(p1), update(p2))
+        case _: PartiallyAppliedCStmtProducer | _: BranchCStmtProducer if isBase(curr) => curr.partApply(steps.head)
+        case _ => curr
+      }
+      op(item, update(bp))
+    }
+  }
+}
--- src/main/scala/org/tygus/suslik/certification/targets/coq/translation/ProgramTranslation.scala
+++ src/main/scala/org/tygus/suslik/certification/targets/coq/translation/ProgramTranslation.scala
@@ -2,104 +2,80 @@ package org.tygus.suslik.certification.targets.coq.translation
 
 import org.tygus.suslik.certification.CertTree
 import org.tygus.suslik.certification.targets.coq.language.Expressions._
+import org.tygus.suslik.certification.targets.coq.language.StatementProducers._
 import org.tygus.suslik.certification.targets.coq.language.Statements._
 import org.tygus.suslik.certification.targets.coq.logic.Proof._
 import org.tygus.suslik.certification.targets.coq.translation.Translation._
 import org.tygus.suslik.language.Statements._
 import org.tygus.suslik.synthesis._
-import org.tygus.suslik.synthesis.rules.FailRules.AbduceBranch
-import org.tygus.suslik.synthesis.rules.LogicalRules._
-import org.tygus.suslik.synthesis.rules.OperationalRules._
-import org.tygus.suslik.synthesis.rules.UnfoldingRules._
 
 object ProgramTranslation {
-  type CStmtProducer = Producer[CStatement]
-
-  private case class TraversalItem(node: CertTree.Node, stmt: Statement) extends Traversable
+  private case class TraversalItem(node: CertTree.Node) extends Traversable
 
   def translate(node: CertTree.Node, proc: Procedure, goal: CGoal): CStatement = {
-    val traversalItem = TraversalItem(node, proc.body)
-    traverseStmt(traversalItem, _.head)
+    val traversalItem = TraversalItem(node)
+    traverseStmt(traversalItem, IdCStmtProducer)
   }
 
-  private def joinCStmtProducer(steps: List[TraversalItem], kont: CStmtProducer): CStmtProducer =
-    joinProducer[CStatement, TraversalItem](traverseStmt)(steps, kont)
-
-  private def seqCompProducer(s: CStatement): CStmtProducer = {
-    case hd :: _ => CSeqComp(s, hd)
-    case Nil => s
-  }
-
-  private def openProducer(selectors: Seq[CExpr]): CStmtProducer = stmts =>
-    if (stmts.length == 1) stmts.head else {
-      val cond_branches = selectors.zip(stmts).reverse
-      val ctail = cond_branches.tail
-      val finalBranch = cond_branches.head._2
-      ctail.foldLeft(finalBranch) { case (eb, (c, tb)) => CIf(c, tb, eb).simplify }
+  def traverseStmt(item: TraversalItem, kont: CStmtProducer): CStatement = {
+    def translateOperation(s: Statement): CStatement = s match {
+      case Skip =>
+        CSkip
+      case Load(to, tpe, from, offset) =>
+        CLoad(CVar(to.name), translateSSLType(tpe), CVar(from.name), offset)
+      case Store(to, offset, e) =>
+        CStore(CVar(to.name), offset, translateExpr(e))
+      case Malloc(to, tpe, sz) =>
+        CMalloc(CVar(to.name), translateSSLType(tpe), sz)
+      case Free(v) =>
+        val block = item.node.consume.pre.blocks.find(_.loc == v)
+        assert(block.nonEmpty)
+        CFree(CVar(v.name), block.get.sz)
+      case Call(v, args, _) =>
+        CCall(CVar(v.name), args.map(translateExpr))
     }
 
-  private def guardedProducer(cond: CExpr): CStmtProducer = stmts =>
-    CGuarded(cond, stmts.head, stmts.last)
-
-  private def deriveCStmtProducer(item: TraversalItem, currStmt: Option[Statement]): Option[CStmtProducer] = {
-    val node = item.node
-
-    val footprint = node.consume
-    val stmtProducer = unwrapStmtProducer(node.kont)
-    (node.rule, stmtProducer) match {
-      case (EmpRule, _) =>
-        Some(seqCompProducer(CSkip))
-      case (ReadRule, PrependProducer(Load(to, _, _, _))) =>
-        currStmt match {
-          case Some(Load(to1, tpe, from, offset)) if to.name.startsWith(to1.name) =>
-            val s = CLoad(CVar(to.name), translateSSLType(tpe), CVar(from.name), offset)
-            Some(seqCompProducer(s))
-          case _ =>
-            None // bound variable was eliminated by SeqComp.simplify
-        }
-      case (WriteRule, PrependProducer(Store(to, offset, e))) =>
-        val s = CStore(CVar(to.name), offset, translateExpr(e))
-        Some(seqCompProducer(s))
-      case (FreeRule, PrependProducer(Free(v))) =>
-        footprint.pre.blocks.find(_.loc == v).map { b =>
-          val s = (1 until b.sz).foldLeft[CStatement](CFree(CVar(v.name))){
-            (acc, n) => CSeqComp(CFree(CVar(v.name), n), acc)
-          }
-          seqCompProducer(s)
-        }
-      case (CallRule, PrependProducer(Call(fun, args, _))) =>
-        val s = CCall(CVar(fun.name), args.map(translateExpr))
-        val cstmt = seqCompProducer(s)
-        Some(cstmt)
-      case (Open, BranchProducer(selectors)) =>
-        val cstmt = openProducer(selectors.map(translateExpr))
-        Some(cstmt)
-      case (AbduceBranch, GuardedProducer(cond, _)) =>
-        val cstmt = guardedProducer(translateExpr(cond))
-        Some(cstmt)
+    def translateProducer(stmtProducer: StmtProducer): CStmtProducer = stmtProducer match {
+      case ChainedProducer(p1, p2) =>
+        val k1 = translateProducer(p1)
+        val k2 = translateProducer(p2)
+        k1 >> k2
+      case PartiallyAppliedProducer(p, _) =>
+        translateProducer(p)
+      case ConstProducer(s) =>
+        val stmt = translateOperation(s)
+        ConstCStmtProducer(stmt)
+      case PrependProducer(s) =>
+        val stmt = translateOperation(s)
+        PrependCStmtProducer(stmt)
+      case BranchProducer(selectors) =>
+        BranchCStmtProducer(selectors.map(translateExpr))
       case _ =>
-        None // rule has no effect on certification
+        IdCStmtProducer
     }
-  }
 
-  @scala.annotation.tailrec
-  private def traverseStmt(item: TraversalItem, kont: CStmtProducer): CStatement = {
-    val (currStmt, nextStmts) = expandStmt(item.stmt)
-    val childNodes = item.node.children
-    val (nextTraversalItems, nextKont) = deriveCStmtProducer(item, currStmt) match {
-      case Some(nextStmtKont) =>
-        val nextTraversalItems = childNodes.zip(nextStmts).map(i => TraversalItem(i._1, i._2))
-        val nextKont = composeProducer[CStatement](nextStmtKont, kont)
-        (nextTraversalItems, nextKont)
+    def generateNextItems: Seq[TraversalItem] =
+      item.node.children.map(n => TraversalItem(n)) // TODO: remove stmt
+
+    def updateProducerPost(nextItems: Seq[TraversalItem], nextKont: CStmtProducer): CStmtProducer = nextKont match {
+      case _: BranchCStmtProducer =>
+        nextItems.tail.foldLeft(nextKont >> kont) {
+          case (foldedP, item) => FoldCStmtProducer(traverseStmt, item, foldedP)
+        }
       case _ =>
-        (childNodes.map(TraversalItem(_, item.stmt)), kont)
+        nextKont >> kont
     }
 
-    nextTraversalItems match {
-      case hd :: tl =>
-        traverseStmt(hd, joinCStmtProducer(tl, nextKont))
-      case Nil =>
-        nextKont(List.empty)
+    // generated nested continuations for children
+    val p = translateProducer(item.node.kont).simplify
+    val nextItems = generateNextItems
+    val nextKont = updateProducerPost(nextItems, p)
+
+    nextItems.headOption match {
+      case Some(childHead) =>
+        traverseStmt(childHead, nextKont)
+      case None =>
+        nextKont(Nil)
     }
   }
 }
