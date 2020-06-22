a426aac0d15628c06c27acdf50bbafd9b2ed7d8e
peleghila <peleg.hila@gmail.com>
2020-06-22 08:56:49 -0700

    setting up for CVC4 synthesis

---
+++ src/main/scala/org/tygus/suslik/synthesis/rules/PureSynthesis.scala
@@ -0,0 +1,16 @@
+package org.tygus.suslik.synthesis.rules
+
+import org.tygus.suslik.logic.Specifications
+import org.tygus.suslik.logic.Specifications.Goal
+
+object PureSynthesis {
+  def toSMTTask(goal: Specifications.Goal): String = {
+    val sb = new StringBuilder
+    ???
+  }
+  def apply(goal: Specifications.Goal): Option[Goal] = {
+
+    None
+  }
+
+}
---
+++ src/test/scala/org/tygus/suslik/synthesis/CVC4Tests.scala
@@ -0,0 +1,83 @@
+package org.tygus.suslik.synthesis
+
+import org.scalatest.FunSuite
+import org.tygus.suslik.language.Expressions.{BinaryExpr, Expr, IntConst, SetLiteral, UnaryExpr}
+import org.tygus.suslik.language.{Expressions, IntSetType, IntType, LocType, Statements}
+import org.tygus.suslik.logic.{Environment, Gamma, PFormula, PointsTo, SFormula}
+import org.tygus.suslik.logic.Specifications.{Assertion, Goal, GoalLabel}
+import org.tygus.suslik.parsing.SSLParser
+import org.tygus.suslik.synthesis.rules.PureSynthesis
+import org.tygus.suslik.synthesis.rules.UnificationRules.Pick
+import org.tygus.suslik.util.SynStats
+
+class CVC4Tests extends FunSuite with SynthesisRunnerUtil {
+  val params: SynConfig = defaultConfig
+  //loc r, int x, int y [int m] |-
+  //{not (r == 0) && x < y ; r :-> 0}
+  //  ??
+  //{x <= m && y <= m ; r :-> m}
+  val goal1 = Goal(
+    Assertion(PFormula(Set[Expr](UnaryExpr(Expressions.OpNot,BinaryExpr(Expressions.OpEq,Expressions.Var("r"),IntConst(0))),
+      BinaryExpr(Expressions.OpLt,Expressions.Var("x"),Expressions.Var("y")))),
+      SFormula(List(PointsTo(Expressions.Var("r"),0,IntConst(0))))), //pre
+    Assertion(PFormula(Set[Expr](BinaryExpr(Expressions.OpLeq,Expressions.Var("x"),Expressions.Var("m")),
+      BinaryExpr(Expressions.OpLeq,Expressions.Var("y"),Expressions.Var("m")))),
+      SFormula(List(PointsTo(Expressions.Var("r"),0,Expressions.Var("m"))))), //post
+    Map(Expressions.Var("r") -> LocType,
+      Expressions.Var("x") -> IntType,
+      Expressions.Var("y") -> IntType,
+      Expressions.Var("m") -> IntType),
+    List(Expressions.Var("r"), Expressions.Var("x"),Expressions.Var("y")),
+    Set(),
+    "maxish",
+    GoalLabel(List(1), Nil),
+    None,
+    Environment(Map.empty, Map.empty, params, new SynStats(params.timeOut)),
+    Statements.Hole,
+    false,
+    false
+  )
+
+  test("Translate ints to SYGUS") {
+    val goal = goal1
+    val smtTask = PureSynthesis.toSMTTask(goal)
+  }
+  test("All ints, one existential") {
+
+    val goal = goal1
+
+    val res = PureSynthesis(goal)
+    //res.map(_.subgoals.head.pp + "\n").foreach(println)
+    assert(!res.isEmpty)
+    val resGoal: Goal = res.get
+    assert(goal.pp == """loc r, int x, int y [] |-
+                        |{not (r == 0) && x < y ; r :-> 0}
+                        |  ??
+                        |{x <= y && y <= y ; r :-> y}""".stripMargin)
+  }
+
+  test("Goal with set and int") {
+    // {true ; emp}??{{v} ++ S1 =i {v1} ++ S11 ; emp}
+    val goal = Goal(
+      Assertion(PFormula(Set[Expr]()),SFormula(Nil)), //pre
+      Assertion(PFormula(Set[Expr](BinaryExpr(Expressions.OpSetEq,BinaryExpr(Expressions.OpUnion,SetLiteral(List(Expressions.Var("v"))),Expressions.Var("S1")),
+                                                                  BinaryExpr(Expressions.OpUnion, SetLiteral(List(Expressions.Var("v1"))),Expressions.Var("S11"))))),
+        SFormula(Nil)), //post
+      Map(Expressions.Var("x") -> LocType,
+        Expressions.Var("S1") -> IntSetType,
+        Expressions.Var("v") -> IntType,
+        Expressions.Var("S11") -> IntSetType,
+        Expressions.Var("S") -> IntSetType,
+        Expressions.Var("v1") -> IntType),
+      List(Expressions.Var("x")),
+      Set(Expressions.Var("S"), Expressions.Var("v"), Expressions.Var("S1")),
+      "foo",
+      GoalLabel(List(1), Nil),
+      None,
+      Environment(Map.empty, Map.empty, params,new SynStats(params.timeOut)),
+      Statements.Hole,
+      false,
+      false
+    )
+  }
+}
