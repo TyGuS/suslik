5bfb3fad106a51011bb9112a333783ba5ae83652
Yasunari Watanabe <yasuwata27@gmail.com>
2021-01-13 20:14:27 +0800

    HTT: move the pretty-printing logic inside the certificate

--- src/main/scala/org/tygus/suslik/certification/targets/htt/HTT.scala
+++ src/main/scala/org/tygus/suslik/certification/targets/htt/HTT.scala
@@ -10,32 +10,12 @@ object HTT extends CertificationTarget {
   val name: String = "HTT"
   val suffix: String = ".v"
 
-  // Import Coq dependencies
-  private val prelude = s"""From mathcomp
-Require Import ssreflect ssrbool ssrnat eqtype seq ssrfun.
-From fcsl
-Require Import prelude pred pcm unionmap heap.
-From HTT
-Require Import stmod stsep stlog stlogR.
-From SSL
-Require Import core.
-
-"""
-
   def certify(proc: Procedure, env: Environment): HTTCertificate = {
     val root = CertTree.root.getOrElse(throw TranslationException("Search tree is uninitialized"))
-    val builder = new StringBuilder
-    builder.append(prelude)
     val (preds, spec, proof, cproc) = Translation.translate(root, proc)(env)
-    preds.values.foreach(pred => builder.append(pred.pp + "\n"))
-    builder.append(spec.pp)
-    builder.append("\n")
-    builder.append(cproc.pp)
-    builder.append("\n")
-    builder.append(proof.pp)
 
     CertTree.clear() // [Certify]: Clear tree after certification complete
 
-    HTTCertificate(builder.toString(), proc.name)
+    HTTCertificate(proc.name, preds, spec, proof, cproc)
   }
 }
