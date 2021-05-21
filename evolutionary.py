fbed3d3d98292d04dba8cd5f57fe7833ad5b9a8f
yutakang <yncyn@nus.edu.sg>
2021-05-21 16:03:16 +0800

    write a new file from Python.

---
+++ evolutionary.py
@@ -0,0 +1,49 @@
+import array
+import random
+import json
+from subprocess import call
+
+import numpy
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+with open("src/main/scala/org/tygus/suslik/synthesis/tactics/orderOfRules.json", "r") as jsonFile:
+    jsonData = json.load(jsonFile)
+
+numbOfAnyPhaseRules = jsonData["numbOfAnyPhaseRules"]
+orderOfAnyPhaseRules = jsonData["orderOfAnyPhaseRules"]
+
+reversedOrderOfAnyPhaseRules = orderOfAnyPhaseRules.copy()
+reversedOrderOfAnyPhaseRules.reverse()
+
+newJsonDataToWrite = {
+  "numbOfAnyPhaseRules" : numbOfAnyPhaseRules,
+  "orderOfAnyPhaseRules": reversedOrderOfAnyPhaseRules
+}
+
+with open("src/main/scala/org/tygus/suslik/synthesis/tactics/orderOfRulesNew.json", 'w') as newJsonFileToWrite:
+  json.dump(newJsonDataToWrite, newJsonFileToWrite)
+
+with open("src/main/scala/org/tygus/suslik/synthesis/tactics/orderOfRulesNew.json", "r") as newJsonFileToRead:
+  newJsonDataToRead = json.load(newJsonFileToRead)
+
+newNumbOfAnyPhaseRules  = newJsonDataToRead["numbOfAnyPhaseRules"]
+newOrderOfAnyPhaseRules = newJsonDataToRead["orderOfAnyPhaseRules"]
+
+def main():
+    print("read a rule-odering from a json file")
+    print(numbOfAnyPhaseRules)
+    print(orderOfAnyPhaseRules)
+    print("reverse")
+    print(reversedOrderOfAnyPhaseRules)
+    print("write the reversed order to a json file then read it")
+    print(newNumbOfAnyPhaseRules)
+    print(newOrderOfAnyPhaseRules)
+    call(['java', '-jar', 'target/scala-2.12/suslik.jar', 'src/test/resources/synthesis/paper-benchmarks/ints/swap.syn', '-t=120000'])
+    return 0
+
+if __name__ == "__main__":
+    main()
