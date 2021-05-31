b12734b6599f409f26b97c721a9d4ed36047e4ac
Shachar Itzhaky <corwin.amber@gmail.com>
2021-05-31 19:40:17 +0300

    [bugfix] Don't disable all loggers worldwide.
    When calling `smt.disableLogging`.
    Disable only SMT-related loggers.

--- src/main/scala/org/tygus/suslik/logic/smt/package.scala
+++ src/main/scala/org/tygus/suslik/logic/smt/package.scala
@@ -1,26 +1,26 @@
 package org.tygus.suslik.logic
 
-import org.bitbucket.franck44.scalasmt.configurations.SMTInit
-import org.bitbucket.franck44.scalasmt.configurations.SMTLogics.QF_LIA
-import org.bitbucket.franck44.scalasmt.configurations.SMTOptions.MODELS
-import org.bitbucket.franck44.scalasmt.interpreters.SMTSolver
-
 /**
   * @author Ilya Sergey
   */
 
 package object smt {
 
+  val LOGGER_NAMES = List("org.bitbucket.franck44", "smt2",
+    "org.tygus.suslik.logic.smt", "class org.tygus.suslik.logic.smt")
+
   def disableLogging()  {
-      org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+    for (name <- LOGGER_NAMES)
+      org.slf4j.LoggerFactory.getLogger(name)
           .asInstanceOf[ch.qos.logback.classic.Logger]
           .setLevel(ch.qos.logback.classic.Level.WARN)
-    }
+  }
 
   def infoLogging()  {
-      org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+    for (name <- LOGGER_NAMES)
+      org.slf4j.LoggerFactory.getLogger(name)
           .asInstanceOf[ch.qos.logback.classic.Logger]
           .setLevel(ch.qos.logback.classic.Level.INFO)
-    }
+  }
 
 }
